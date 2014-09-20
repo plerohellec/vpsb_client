c6ffc273795b7236400744542c34798a48067f16
Philippe Le Rohellec <philippe@lerohellec.com>
2014-09-20 15:56:52 -0700

    tests for GetCurrentTrialRequest

--- spec/lib/request_spec.rb
+++ spec/lib/request_spec.rb
@@ -2,86 +2,108 @@ require 'spec_helper'
 
 module VpsbClient
   module Api
-    support_dir = File.join(File.dirname(__FILE__), '..', 'support/')
+    describe 'Requests' do
+      support_dir = File.join(File.dirname(__FILE__), '..', 'support/')
 
-    describe GetCurrentTrialRequest do
-      before :each do
-      end
-
-      it 'sends a get request' do
-        client = double('client')
-        req = GetCurrentTrialRequest.new(client, 1, 2, 3)
-        expect(client).to receive(:get).with(req).once
-        req.run
-      end
-    end
-
-    describe GetItemIdRequest do
-      it 'url includes item type and name' do
-        curl = double('curl')
-        client = HttpClient.new(curl, 'http', 'localhost')
-        expect(curl).to receive(:get).with('http://localhost/admin/hosters/by_name/linode.json').once
-
-        req = GetItemIdRequest.new(client, 'hosters', 'linode')
-        req.run
-      end
-
-      it 'parses the response from the server' do
-        curl = double('curl')
-        client = HttpClient.new(curl, 'http', 'localhost')
-
-        curl_response = double('response')
-        allow(curl_response).to receive(:response_code).and_return(200)
-        allow(curl_response).to receive(:body_str).and_return('[{"id": 25}]')
-        allow(curl_response).to receive(:content_type).and_return("application/json")
-        allow(curl).to receive(:get).and_return(curl_response)
-        req = GetItemIdRequest.new(client, 'hosters', 'linode')
-        resp = Response.new(req.run)
-        expect(GetItemIdRequest.item_id(resp)).to eq(25)
-      end
-    end
-
-    describe SigninRequest do
-      it 'url is /users/signin' do
-        curl = double('curl')
-        client = HttpClient.new(curl, 'http', 'localhost')
-        expect(curl).to receive(:post).with('http://localhost/users/sign_in',
-                                           {"user[email]"=>"foo@bar.com", "user[password]"=>"foobar", :authenticity_token=>"xyz"},
-                                           "application/x-www-form-urlencoded").once
-
-        req = SigninRequest.new(client, 'foo@bar.com', 'foobar', 'xyz')
-        req.run
-      end
-    end
-
-    describe CreateTrialRequest do
       before :each do
         @curl = double('curl')
         @client = HttpClient.new(@curl, 'http', 'localhost')
-        @params = Builders::Trial.new(1, 1, 1, 'foo bar').params
-        @csrf_token = 'abc'
-        @trial_params = { 'trial' => @params, 'authenticity_token' => @csrf_token }
       end
 
-      it 'url id /admin/trials' do
-        expect(@curl).to receive(:post).with('http://localhost/admin/trials.json',
-                                            @trial_params.to_json,
-                                            "application/json").once
+      describe GetItemIdRequest do
+        it 'url includes item type and name' do
+          expect(@curl).to receive(:get).with('http://localhost/admin/hosters/by_name/linode.json').once
 
-        req = CreateTrialRequest.new(@client, @params, @csrf_token)
-        req.run
+          req = GetItemIdRequest.new(@client, 'hosters', 'linode')
+          req.run
+        end
+
+        it 'parses the response from the server' do
+          curl_response = double('response')
+          allow(curl_response).to receive(:response_code).and_return(200)
+          allow(curl_response).to receive(:body_str).and_return('[{"id": 25}]')
+          allow(curl_response).to receive(:content_type).and_return("application/json")
+          allow(@curl).to receive(:get).and_return(curl_response)
+          req = GetItemIdRequest.new(@client, 'hosters', 'linode')
+          resp = Response.new(req.run)
+          expect(GetItemIdRequest.item_id(resp)).to eq(25)
+        end
       end
 
-      it 'parses the response from the server' do
-        curl_response = double('response')
-        allow(curl_response).to receive(:response_code).and_return(200)
-        allow(curl_response).to receive(:body_str).and_return('{"id": 8, "application_id": 1}')
-        allow(curl_response).to receive(:content_type).and_return("application/json")
-        allow(@curl).to receive(:post).and_return(curl_response)
-        req = CreateTrialRequest.new(@client, @params, @csrf_token)
-        resp = Response.new(req.run)
-        expect(CreateTrialRequest.trial_id(resp)).to eq(8)
+      describe SigninRequest do
+        it 'url is /users/signin' do
+          expect(@curl).to receive(:post).with('http://localhost/users/sign_in',
+                                            {"user[email]"=>"foo@bar.com", "user[password]"=>"foobar", :authenticity_token=>"xyz"},
+                                            "application/x-www-form-urlencoded").once
+
+          req = SigninRequest.new(@client, 'foo@bar.com', 'foobar', 'xyz')
+          req.run
+        end
       end
+
+      describe CreateTrialRequest do
+        before :each do
+          @params = Builders::Trial.new(1, 1, 1, 'foo bar').params
+          @csrf_token = 'abc'
+          @trial_params = { 'trial' => @params, 'authenticity_token' => @csrf_token }
+        end
+
+        it 'url id /admin/trials' do
+          expect(@curl).to receive(:post).with('http://localhost/admin/trials.json',
+                                              @trial_params.to_json,
+                                              "application/json").once
+
+          req = CreateTrialRequest.new(@client, @params, @csrf_token)
+          req.run
+        end
+
+        it 'parses the response from the server' do
+          curl_response = double('response')
+          allow(curl_response).to receive(:response_code).and_return(200)
+          allow(curl_response).to receive(:body_str).and_return('{"id": 8, "application_id": 1}')
+          allow(curl_response).to receive(:content_type).and_return("application/json")
+          allow(@curl).to receive(:post).and_return(curl_response)
+          req = CreateTrialRequest.new(@client, @params, @csrf_token)
+          resp = Response.new(req.run)
+          expect(CreateTrialRequest.trial_id(resp)).to eq(8)
+        end
+      end
+
+      describe GetCurrentTrialRequest do
+        before :each do
+          @params = { application_id: 1, plan_id: 1, hoster_id: 1 }
+        end
+
+        it 'gets /admin/trials/current with ids' do
+          expect(@curl).to receive(:get).with('http://localhost/admin/trials/current.json?hoster_id=1&application_id=1&plan_id=1').once
+
+          req = GetCurrentTrialRequest.new(@client, @params)
+          req.run
+        end
+
+        it 'extracts current trial id from response if found' do
+          curl_response = double('response')
+          allow(curl_response).to receive(:response_code).and_return(200)
+          allow(curl_response).to receive(:body_str).and_return('[{"id": 8, "application_id": 1}]')
+          allow(curl_response).to receive(:content_type).and_return("application/json")
+          allow(@curl).to receive(:get).and_return(curl_response)
+          req = GetCurrentTrialRequest.new(@client, @params)
+          resp = Response.new(req.run)
+          expect(GetCurrentTrialRequest.trial_id(resp)).to eq(8)
+        end
+
+        it 'extracts ni from response if notfound' do
+          curl_response = double('response')
+          allow(curl_response).to receive(:response_code).and_return(200)
+          allow(curl_response).to receive(:body_str).and_return('[]')
+          allow(curl_response).to receive(:content_type).and_return("application/json")
+          allow(@curl).to receive(:get).and_return(curl_response)
+          req = GetCurrentTrialRequest.new(@client, @params)
+          resp = Response.new(req.run)
+          expect(GetCurrentTrialRequest.trial_id(resp)).to be_nil
+        end
+      end
+
     end
   end
 end
