b96d92dd3648e376a42bd7fd1407682105975511
Philippe Le Rohellec <philippe@lerohellec.com>
2014-09-21 12:39:55 -0700

    SarManager produces sar files tht can be processed by LogfileInterval

---
+++ lib/vpsb_client/datafiles/sar_manager.rb
@@ -0,0 +1,62 @@
+module VpsbVclient
+  module Datafiles
+    class SarManager
+      class NotFoundError < StandardError; end
+      class PermissionDeniedError < StandardError; end
+      class SadfError < StandardError; end
+
+      SADF = '/usr/bin/sadf'
+
+      def initialize(orig_path, target_path)
+        raise NotFoundError, "#{orig_path} is not a directory" unless File.directory?(orig_path)
+        raise NotFoundError unless File.executable?(SADF)
+        @orig_path = orig_path
+        @target_path = target_path
+      end
+
+      def run
+        create_target_path
+        create_daily_formatted
+        create_current_day_temp_formatted
+      end
+
+      private
+      def create_target_path
+        if File.directory?(@target_path)
+          raise PermissionDeniedError, "#{@target_path} is not writable" unless File.writable?(@target_path)
+          return
+        end
+        raise PermissionDeniedError unless File.writable?(File.dirname(@target_path), 0755)
+        Dir.mkdir(@target_path)
+      end
+
+      def create_daily_formatted
+        raw_sar_filenames = Dir.glob("#{@orig_path}/sa*")
+        raw_sar_filenames.each do |filename|
+          filename.match /sa(?<num>\d+)$/ do |matchdata|
+            fileday = matchdata[:num]
+            next if fileday.to_i == Time.now.day
+
+            formatted_filename = "#{@target_path}/formatted_sa#{fileday}"
+            next if File.exist?(formatted_filename)
+            puts "Formatting sar file to #{formatted_filename}"
+            sadf(filename, formatted_filename)
+          end
+        end
+      end
+
+      def create_current_day_temp_formatted
+        sa_filename = "#{@orig_path}/sa#{'%02d' % Time.now.day}"
+        formatted_filename = "#{@target_path}/formatted_sa#{'%02d' % Time.now.day}"
+        File.delete(formatted_filename) if File.exist?(formatted_filename)
+        sadf(sa_filename, formatted_filename)
+      end
+
+      def sadf(src, dest)
+        cmd = "#{SADF} -d #{src} -U > #{dest}"
+        ret = system cmd
+        raise SadfError, "\"#{cmd}\" failed" unless ret
+      end
+    end
+  end
+end
