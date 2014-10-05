945e994cced8232320e728a705662988596a5083
Philippe Le Rohellec <philippe@lerohellec.com>
2014-10-05 16:24:24 -0700

    more tests for metrics interval builder

--- spec/lib/metrics_interval_spec.rb
+++ spec/lib/metrics_interval_spec.rb
@@ -8,19 +8,46 @@ module VpsbClient
       before :each do
         @sar_dir = "#{support_dir}/sarfiles/formatted"
         @timing_dir = "#{support_dir}/timingfiles"
-        @builder = MetricsInterval.new(@sar_dir, @timing_dir, Time.new(2014,9,20,12,0,0), 3600)
-        allow(Time).to receive(:now).and_return(Time.new(2014,9,21,16,0,1))
+        @builder = MetricsInterval.new(@sar_dir, @timing_dir, Time.new(2014,9,19,12,0,0), 3600)
+        allow(Time).to receive(:now).and_return(Time.new(2014,9,21,14,0,1))
       end
 
-      it 'should yield 27 times' do
-        expect { |b| @builder.each(&b) }.to yield_control.at_least(27)
+      it 'should yield 37 times' do
+        expect { |b| @builder.each(&b) }.to yield_control.at_least(37)
       end
 
       context 'first interval' do
-        it 'goes in descending order' do
+        it 'is the most recent' do
           interval_enum = @builder.each
-          expect(interval_enum.next[:start_time]).to eq(Time.new(2014,9,21,15,0,0))
-          expect(interval_enum.next[:start_time]).to eq(Time.new(2014,9,21,14,0,0))
+          expect(interval_enum.next[:start_time]).to eq(Time.new(2014,9,21,13,0,0))
+          expect(interval_enum.next[:start_time]).to eq(Time.new(2014,9,21,12,0,0))
+        end
+
+        it 'has the requested length' do
+          i = @builder.each.first
+          expect(i[:duration_seconds]).to eq(3600)
+        end
+
+        it 'has the right averaged timing' do
+          i = @builder.each.first
+          expect(i[:start_time]).to eq(Time.new(2014,9,21,13,0,0))
+          expect(i[:resptime_total_ms]).to eq(29.5)
+        end
+
+        it 'as the right p95 cpu idle' do
+          i = @builder.each.first
+          expect(i[:p99_cpuidle_pct]).to eq(88.0)
+        end
+      end
+
+      context 'last interval' do
+        it 'starts at the oldest data point' do
+          last_interval = nil
+          @builder.each do |i|
+            last_interval = i
+          end
+          expect(last_interval[:start_time]).to eq(Time.new(2014,9,20,0,0,0))
+          expect(last_interval[:duration_seconds]).to eq(3600)
         end
       end
     end
